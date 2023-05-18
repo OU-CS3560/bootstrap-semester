02c7b54470aaecdee2b391e433fb341b3865708a
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2023-05-18 01:30:07 -0400

    feat: draft the frontend, add dockerfile for the frontend

--- frontend/app/src/main.jsx
+++ frontend/app/src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App.jsx";
+import Classroom from "./routes/classroom.jsx";
+import Team from "./routes/team.jsx";
+import ErrorPage from "./error-page.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/hello-world",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/classroom/:classromId",
+    element: <Classroom />,
+  },
+  {
+    path: "/team/:teamId",
+    element: <Team />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ frontend/app/src/routes/team.jsx
@@ -0,0 +1,56 @@
+export default function Team() {
+  const team = {
+    name: "Bobcat CS",
+    project_brief: "Web application using React and FastAPI",
+    technology_platform_description: "Python, JavaScript",
+    members: [
+      {
+        first_name: "A",
+        last_name: "a",
+        username: "aa",
+        github_username: "aa",
+      },
+      {
+        first_name: "B",
+        last_name: "b",
+        username: "bb",
+        github_username: "bb",
+      },
+      {
+        first_name: "C",
+        last_name: "c",
+        username: "cc",
+        github_username: "cc",
+      },
+      {
+        first_name: "D",
+        last_name: "d",
+        username: "dd",
+        github_username: "d",
+      },
+    ],
+  };
+
+  return (
+    <div id="team">
+      <section id="team-info">
+        <h1>{team.name}</h1>
+      </section>
+      <section id="members-list">
+        <h3>Members</h3>
+        <p>{team.project_brief}</p>
+        <p>{team.technology_platform_description}</p>
+        <ul>
+          {team.members &&
+            team.members.map((student, index) => (
+              <li key={index}>
+                <a href={`/student/${student.id}`}>
+                  {student.first_name} {student.last_name}
+                </a>
+              </li>
+            ))}
+        </ul>
+      </section>
+    </div>
+  );
+}
