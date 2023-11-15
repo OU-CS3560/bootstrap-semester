dc102f5d0299f00d8fed21e2a1ffcda019bb37ca
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2023-11-15 19:55:05 +0000

    feat: experiment with chunk splitting

--- frontend/app/vite.config.js
+++ frontend/app/vite.config.js
@@ -1,10 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     sourcemap: "inline",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          console.log(id);
+          if (id.includes("react-router-dom") || id.includes("react-router")) {
+            return '@react-router';
+          } else if (id.includes("bootstrap") || id.includes("popperjs")) {
+            return "@bootstrap";
+          } else if (id.includes("axios")) {
+            return "@axios";
+          } else if (id.includes("react")) {
+            return "@react";
+          }
+        }
+      }
+    }
   },
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
 });
