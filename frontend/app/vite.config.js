01b1a413a44eb65a9450b0f9ceb02a68858c1b0e
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2023-11-15 20:49:17 +0000

    fix: chunks result in errors
    The splitting messed up the dependencies.

--- frontend/app/vite.config.js
+++ frontend/app/vite.config.js
@@ -5,21 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   build: {
     sourcemap: "inline",
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("react-router-dom") || id.includes("react-router")) {
-            return '@react-router';
-          } else if (id.includes("bootstrap") || id.includes("popperjs")) {
-            return "@bootstrap";
-          } else if (id.includes("axios")) {
-            return "@axios";
-          } else if (id.includes("react")) {
-            return "@react";
-          }
-        }
-      }
-    }
   },
   plugins: [react(), splitVendorChunkPlugin()],
 });
